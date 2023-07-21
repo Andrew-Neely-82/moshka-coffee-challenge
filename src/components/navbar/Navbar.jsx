d44651acd18009a1578243fd8805abe6f29698d5
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-07-21 17:33:40 -0500

    navbar responsiveness greatly improved, needs more work

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,29 @@
 import { NavbarLeft, NavbarMiddle, NavbarRight } from "./import";
-import './style.scss'
+import { Navbar as NavbarContainer } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import "./style.scss";
 
 const Navbar = () => {
   return (
-    <div className="Navbar__">
-      <NavbarLeft />
-      <NavbarMiddle />
-      <NavbarRight />
-    </div>
+    <NavbarContainer expand="lg" className="Navbar__ bg-body-tertiary navbar-dark ">
+      <Container>
+        <NavbarLeft />
+        <NavbarContainer.Brand>
+          <NavbarMiddle />
+        </NavbarContainer.Brand>
+        <NavbarRight />
+        {/*  */}
+        <NavbarContainer.Toggle aria-controls="basic-navbar-nav" />
+        {/* mobile drop down */}
+        <NavbarContainer.Collapse id="basic-navbar-nav" className="hidden">
+          <Nav className={`me-auto hidden`}>
+            <NavbarLeft />
+            <NavbarRight />
+          </Nav>
+        </NavbarContainer.Collapse>
+      </Container>
+    </NavbarContainer>
   );
 };
 export default Navbar;
